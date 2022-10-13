c6725f3e2eff1f1138b639756861b24aefa2ce1d
Nishachinnannan <100110248+Nishachinnannan@users.noreply.github.com>
2022-10-13 12:37:26 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-id=$1
-sns=$2
+var=$1
 
 terraform init -input=false
-terraform plan -input=false -var=instance=${id} -var=sns_topic_name=${sns} -lock=false
-terraform apply -input=false -var=instance=${id} -var=sns_topic_name=${sns} -auto-approve
+terraform plan -input=false -var-file 'instance=${var}' -lock=false
+terraform apply -input=false -var-file 'instance=${var}' -auto-approve
+
+
+var=$2
+
+terraform init -input=false
+terraform plan -input=false -var-file 'sns_topic_name=${var}' -lock=false
+terraform apply -input=false -var-file 'sns_topic_name=${var}' -auto-approve
