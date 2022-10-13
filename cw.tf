0f0516bb90748df35bcb4e388cec19fd77a7880a
Nishachinnannan <100110248+Nishachinnannan@users.noreply.github.com>
2022-10-13 18:49:30 +0530

    Update cw.tf

--- cw.tf
+++ cw.tf
@@ -1,12 +1,3 @@
-variable "instance" {
-    description = "Instance ID"
-    type = string
- }
-variable "sns_topic_name" {
-  type        = string
-  description = "ARN of SNS topic that will be subscribed to an alarm."
-}
-
 ############### create Cloudwatch alarm ##########################
 resource "aws_cloudwatch_metric_alarm" "test" {
   alarm_name         = format("%s-%s",var.instance,"CPU-UTILIZATION-HIGH")
